2d06daa08172defa5d10dbcfe16cb74770fab1e6
itcast <itcast@itcast.com>
2020-07-24 11:40:48 +0800

    天气查询

--- pages/weather/weather.js
+++ pages/weather/weather.js
@@ -7,36 +7,92 @@ Page({
 	data: {
 		region: ['广东省', '肇庆市', '端州区'],
 		now: {
-			tmp: 0,
-			cond_txt: '未知',
-			cond_code: '999',
-			hum: 0,
-			pres: 0,
+			feelsLike: 0,
+			temp: 0,
+			text: '未知',
+			icon: 999,
+			humidity: 0,
+			pressure: 0,
 			vis: 0,
-			wind_dir: 0,
-			wind_spd: 0,
-			wind_sc: 0
-		}
+			windDir: 0,
+			windSpeed: 0,
+			windScale: 0
+		},
+		locationID: 101280904
 	},
 	regionChange(e) {
-		this.setData({
+		var that = this
+		that.setData({
 			region: e.detail.value
 		})
-		this.getWeather()
+		var province = that.data.region[0]
+		var city = that.data.region[1]
+		var region = that.data.region[2]
+		if (region == '湘潭县') {
+			region = city
+		}
+		wx.request({
+			url: 'https://geoapi.heweather.net/v2/city/lookup',
+			data: {
+				location: region,
+				// adm: city,
+				key: 'cf83e0da03da4aa0b51bc9bdd9b4f2b5'
+			},
+			success(res) {
+
+				if (res.data.status == "200") {
+					let location = res.data.location;
+					for (let i = 0; i < location.length; i++) {
+						let local = location[i]
+						let adm1 = local.adm1
+						let adm2 = local.adm2
+						let name = local.name
+						if (province.indexOf(adm1) > -1 && city.indexOf(adm2) > -1 && region.indexOf(name) > -1) {
+							that.setData({
+								locationID: local.id
+							})
+							that.getWeather()
+							break;
+						}
+					}
+					if (that.data.locationID == null) {
+						wx.showToast({
+							title: '获取城市信息失败',
+							icon: 'none'
+						})
+					}
+				} else {
+					wx.showToast({
+						title: '获取城市信息失败',
+						icon: 'none'
+					})
+				}
+				// console.log(res)
+			}
+		})
+		
 	},
 	getWeather() {
 		var that = this
 		wx.request({
 			url: 'https://devapi.heweather.net/v7/weather/now',
 			data: {
-				location: 101010100,
+				location: that.data.locationID,
 				key: 'cf83e0da03da4aa0b51bc9bdd9b4f2b5'
 			},
 			success(res) {
-				console.log(res.data)
-				that.setData({
-					now: res.data.now
-				})
+				if (res.data.code == "200") {
+					that.setData({
+						now: res.data.now
+					})
+				} else {
+					wx.showToast({
+						title: '获取城市天气信息失败',
+						icon: 'none'
+					})
+				}
+				// console.log(res.data)
+
 			}
 		})
 	},
